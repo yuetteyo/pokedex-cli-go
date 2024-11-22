bd79bfe14cce02d9f8b50cc06b5b8f91fba4ed23
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-11-22 17:44:53 +0300

    feat: pokeapi added

---
+++ internal/pokeapi/types_location_area.go
@@ -0,0 +1,13 @@
+package pokeapi
+
+
+// Структура для декодирования json
+type LocationAreasResp struct {
+	Count    int    `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/yuetteyo/pokedex-cli-go/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	// startRepl()
+
+	pokeapiClient := pokeapi.NewClient()
+
+	resp, err := pokeapiClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp)
+
 }
