c397e3e5925dded46c94335bf9cb7e98a22c5f02
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-11-23 19:07:21 +0300

    feat: reap function to delete cache implemented

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -11,10 +11,12 @@ type cacheEntry struct {
 	createdAt time.Time
 }
 
-func NewCache() Cache {
-	return Cache{
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
 		cache: make(map[string]cacheEntry),
 	}
+	go c.reapLoop(interval)
+	return c
 }
 
 func (c *Cache) Add(key string, value []byte) {
@@ -27,4 +29,20 @@ func (c *Cache) Add(key string, value []byte) {
 func (c *Cache) Get(key string) ([]byte, bool) {
 	cacheE, ok := c.cache[key]
 	return cacheE.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(interval)
+	}
+}
+
+func (c *Cache) reap(interval time.Duration) {
+	timeAgo := time.Now().UTC().Add(-interval)
+	for k, v := range c.cache {
+		if v.createdAt.Before(timeAgo) {
+			delete(c.cache, k)
+		}
+	}
 }
