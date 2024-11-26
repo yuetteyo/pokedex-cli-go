3af8634fd60f304b9fb388422f7a3af433248f42
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-11-26 12:15:52 +0300

    feat(repl): func catch implemented

---
+++ command_catch.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+
+)
+
+func callbackCatch(cfg *config, args ...string) error {
+
+	if len(args) != 1 {
+		return errors.New("no pokemon provided")
+	}
+
+	pokemonName := args[0]
+
+	pokemon, err := cfg.pokeapiClient.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	const threshold = 50
+	randNum := rand.Intn(pokemon.BaseExperience)
+	if randNum > threshold {
+		return fmt.Errorf("failed to catch %s", pokemonName)
+	}
+
+	fmt.Printf("Pokemon %s was caught!\n", pokemonName)
+
+	return nil
+}
